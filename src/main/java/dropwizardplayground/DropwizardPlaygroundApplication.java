3db8d590791e980b86e616fe6efc38c89fbc7a2c
Patrik Lilja <lilja.patrik@gmail.com>
2022-10-09 16:13:30 +0200

    Fix broken archetype generation

---
+++ src/main/java/dropwizardplayground/DropwizardPlaygroundApplication.java
@@ -0,0 +1,30 @@
+package dropwizardplayground;
+
+import dropwizardplayground.DropwizardPlaygroundConfiguration;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class DropwizardPlaygroundApplication extends Application<DropwizardPlaygroundConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new DropwizardPlaygroundApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "dropwizardplayground";
+    }
+
+    @Override
+    public void initialize(Bootstrap<DropwizardPlaygroundConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(DropwizardPlaygroundConfiguration configuration,
+                    Environment environment) {
+        // TODO: implement application
+    }
+
+}
--- src/main/java/dropwizardplayground/DropwizardPlaygroundConfiguration.java
+++ src/main/java/dropwizardplayground/DropwizardPlaygroundConfiguration.java
@@ -1,10 +1,10 @@
-package dropwizard-playground;
+package dropwizardplayground;
 
 import io.dropwizard.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.validator.constraints.*;
 import javax.validation.constraints.*;
 
-public class dropwizard-playgroundConfiguration extends Configuration {
+public class DropwizardPlaygroundConfiguration extends Configuration {
     // TODO: implement service configuration
 }
